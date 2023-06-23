1638b2b74f2ffeee40601dea400fdb6393d4c140
AlbertoJTD <albertojtd@outlook.com>
2023-06-23 16:52:13 -0600

    Add enfironment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment ={
+  production: true,
+  firestore: {
+    apiKey: "key",
+    authDomain: "domain",
+    projectId: "projectId",
+    storageBucket: "storageBucket",
+    messagingSenderId: "messagingSenderId",
+    appId: "appId"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment ={
+  production: false,
+  firestore: {
+    apiKey: "key",
+    authDomain: "domain",
+    projectId: "projectId",
+    storageBucket: "storageBucket",
+    messagingSenderId: "messagingSenderId",
+    appId: "appId"
+  }
+};
