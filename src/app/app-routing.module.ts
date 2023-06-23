424b1a4a0b196a04087c84fe78c6f4ac2ef1dd5a
AlbertoJTD <albertojtd@outlook.com>
2023-06-23 14:47:05 -0600

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ConfigurationComponent } from './components/configuration/configuration.component';
+import { EditCustomerComponent } from './components/edit-customer/edit-customer.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'configuration', component: ConfigurationComponent },
+  { path: 'customer/edit/:id', component: EditCustomerComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
