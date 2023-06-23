8ac0230a0ac8b34cfdcd13075ecb8215b1fa12c2
AlbertoJTD <albertojtd@outlook.com>
2023-06-23 14:03:03 -0600

    Add app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CustomersComponent } from './components/customers/customers.component';
+import { EditCustomerComponent } from './components/edit-customer/edit-customer.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ConfigurationComponent } from './components/configuration/configuration.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    CustomersComponent,
+    EditCustomerComponent,
+    LoginComponent,
+    SignupComponent,
+    ConfigurationComponent,
+    NotFoundComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
